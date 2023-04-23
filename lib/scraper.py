57bf4072ad981d35ba5ca55607bf3a340eb9a098
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-23 18:27:22 -0500

    d

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,9 @@
-from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+children = doc.select(('div.card-body'))[0].children
+for child in children:
+    print(child)
